0a062e8e895b19a44f35f282f56027581d8d0457
Jesse Marquez <jmarqu1134@gmail.com>
2019-12-17 20:57:53 -0800

    added conditionals
    Will determine if there is citation and year to produce the appropriate string.

--- js/script.js
+++ js/script.js
@@ -15,19 +15,23 @@ project 1 - A Random Quote Generator
 var quotes = [
   {
     quote: 'I still see your shadows in my room Can\'t take back the love that I gave you It\'s to the point where I love and I hate you And I cannot change you so I must replace you (oh)',
-    source: '– Juice World',
+    source: 'Juice World',
+    citation: 'twitter',
+    year: 1996,
   },
   {
     quote: 'Easier said than done, I thought you were the one Listenin to my heart instead of my head',
-    source: '– Juice World',
+    source: 'Juice World',
   },
   {
     quote: 'You left me falling and landing inside my grave I know that you want me dead,I take prescriptions to make me feel a-okay I know it\'s all in my head I have these lucid dreams where I can\'t move a thing Thinking of you in my bed',
-    source: '– Juice World',
+    source: 'Juice World',
+    citation: 'facebook',
   },
   {
     quote: 'You were my everything Thoughts of a wedding ring Now I\'m just better off dead (Uh, uh, uh)',
-    source: '– Juice World',
+    source: 'Juice World',
+    year: 1996,
   },
 ];
 /***
@@ -41,11 +45,25 @@ function getRandomQuote (quote) {
 }
 function generateQuoteString (quoteArray) {
   var tempArray = quoteArray;
-  var quoteString = '<p class="quote">' + tempArray.quote + '</p>';
-  quoteString += '<p class="source">' + tempArray.source + '</p>'
-  console.log(` random string generated: ${quoteString}`); // This is a test to make sure everything is working properly
+  var quoteString = '';
+  quoteString += '<p class="quote">' + tempArray.quote + '</p>';
+  quoteString += '<p class="source">' + tempArray.source;
+
+    if(isNaN(tempArray.citation) && isNaN(tempArray.year)) {
+      console.log('There is no citation and year');
+      quoteString += '</p>';
+    } else if (isNaN(tempArray.citation)) {
+      console.log('There is no Citation');
+      quoteString += '<span class="year">' + tempArray.year + '</span></p>';
+    } else if (isNaN(tempArray.year)) {
+      console.log('There is no year');
+      quoteString += '<span class="citation">' + tempArray.citation + '</span></p>';
+    } else {
+      quoteString += '</p>';
+    }
   return quoteString;
 }
+
 function printQuotes (quoteDisplay) {
   var outputQuote_Box = document.getElementById('quote-box');
   outputQuote_Box.innerHTML = quoteDisplay;
